fd3d205728cd7391452f18f235952ed5fa798bc1
Arman19941113 <td19941113@gmail.com>
2020-06-09 09:53:50 +0800

    Initialize project using Create Web Temp

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 style="margin-top: 128px;text-align: center;">{{msg}}</h1>
+</template>
+
+<script>
+    import { ref } from 'vue'
+
+    export default {
+        setup () {
+            return {
+                msg: ref('Hello world'),
+            }
+        },
+    }
+</script>
+
+<style>
+    @import "~@/css/reset.css";
+    @import "~@/css/variable.css";
+    #app {
+        color: $fontColor;
+    }
+</style>
